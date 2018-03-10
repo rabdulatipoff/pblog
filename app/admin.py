c9adf99812a223f32b2713ab4835bccde7be3d0a
Rustam Abdulatipov <rabdulatipoff@gmail.com>
2018-03-10 11:44:25 +0300

    New CSS style for the template

--- app/admin.py
+++ app/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+def publish_posts(modeladmin, request, queryset):
+    for post in queryset:
+        post.publish()
+publish_posts.short_description = "Publish the selected posts"
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'published_date']
+    ordering = ['published_date']
+    actions = [publish_posts]
+
+admin.site.register(Post, PostAdmin)
