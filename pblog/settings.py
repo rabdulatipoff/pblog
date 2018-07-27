1fc0541efd0053bf7bfcc90ecdfdd46f740044c2
Rustam Abdulatipov <rabdulatipoff@gmail.com>
2018-07-27 15:13:55 +0300

    set ALLOWED_HOSTS to *

--- pblog/settings.py
+++ pblog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '&0o0&h*l5%!648yk^ol^s28_%pxvp!fvk)ojokxfewv=4fgkw7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
