8c1dbde43f1ec5b1f84d56b8e8dc82ec122a8af5
Rustam Abdulatipov <rabdulatipoff@gmail.com>
2018-08-05 02:01:06 +0300

    added post and login urls, media/static files

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from . import views
+from django.conf.urls.static import static
+from django.conf import settings
+
+from django.contrib.auth.views import LoginView
+from . import views, forms
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path('post/<int:pk>', views.post_detail, name='post_detail'),
-    path('post/new', views.post_new, name='post_new'),
-]
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+
+    path('login/', LoginView.as_view(template_name = 'app/login_form.html', authentication_form = forms.LoginForm), name='login'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
