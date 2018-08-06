8c93a6c70ebfd4769c89e229ae7dbdada4e2a084
Rustam Abdulatipov <rabdulatipoff@gmail.com>
2018-08-06 22:17:41 +0300

    added user post wall url

--- app/urls.py
+++ app/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
+    path('user/<str:username>/', views.post_wall, name='post_wall'),
 
     path('login/', LoginView.as_view(template_name = 'app/login_form.html', authentication_form = forms.LoginForm), name='login'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
