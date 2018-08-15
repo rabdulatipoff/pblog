b9503d89c9e0b68dc0176182223978be6faaef7a
Rustam Abdulatipov <rabdulatipoff@gmail.com>
2018-08-15 16:13:52 +0300

    store secrets with python-decouple and git-secret

--- pblog/settings.py
+++ pblog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,10 +21,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&0o0&h*l5%!648yk^ol^s28_%pxvp!fvk)ojokxfewv=4fgkw7'
+# SECRET_KEY = '&0o0&h*l5%!648yk^ol^s28_%pxvp!fvk)ojokxfewv=4fgkw7'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('SECRET_KEY', default = False, cast = bool)
 
 # Enabled '*' for testing, must be configured for production use
 ALLOWED_HOSTS = ['*']
@@ -114,8 +116,8 @@ APPEND_SLASH = True
 
 # ReCaptcha settings
 # Don't forget to set the key values
-RECAPTCHA_PRIVATE_KEY = ''
-RECAPTCHA_PUBLIC_KEY = ''
+RECAPTCHA_PRIVATE_KEY = config('RECAPTCHA_PRIVATE_KEY')
+RECAPTCHA_PUBLIC_KEY = config('RECAPTCHA_PUBLIC_KEY')
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
